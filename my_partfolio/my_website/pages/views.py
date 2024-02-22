da545b901dba134f9ffa0efbdd53323c5d7b7a39
WanGog <46074817+Wangog@users.noreply.github.com>
2024-02-22 08:47:21 +0200

    Hello, world in django
    Yesss

--- my_partfolio/my_website/pages/views.py
+++ my_partfolio/my_website/pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def my_website(request):
-return HttpResponse('Hello, World!')
+
+def homePageView(request):
+    return HttpResponse('Hello, World!')
